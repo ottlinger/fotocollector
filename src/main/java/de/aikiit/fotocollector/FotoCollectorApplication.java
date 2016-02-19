6ce2241f762c7108ec8360cef1c7d2939d32a029
Hugo Hirsch <github@hugo-hirsch.de>
2016-02-19 22:20:45 +0100

    Issue #1: Gradle fixes to get tika running

--- src/main/java/de/aikiit/fotocollector/FotoCollectorApplication.java
+++ src/main/java/de/aikiit/fotocollector/FotoCollectorApplication.java
@@ -1,5 +1,14 @@
 package de.aikiit.fotocollector;
 
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Play around with Tika to extract and find images.
  *
@@ -7,4 +16,17 @@ package de.aikiit.fotocollector;
  * @version 2016-02-19, 22:03
  */
 public class FotoCollectorApplication {
+
+    public static void main(final String[] args) throws IOException,
+            SAXException, TikaException {
+
+        // TODO https://tika.apache.org/1.7/examples.html#Parsing_using_the_Tika_Facade
+        AutoDetectParser parser = new AutoDetectParser();
+        BodyContentHandler handler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+        try (InputStream stream = FotoCollectorApplication.class.getResourceAsStream("test.doc")) {
+            parser.parse(stream, handler, metadata);
+            System.out.println(handler.toString());
+        }
+    }
 }
