898ac1c7bf12dd9f4c46a3e9c1ad5ecc3a5843dc
Hugo Hirsch <github@hugo-hirsch.de>
2016-02-19 22:05:16 +0100

    Issue #1: Project setup.

---
+++ src/main/java/de/aikiit/fotocollector/FotoCollectorApplication.java
@@ -0,0 +1,10 @@
+package de.aikiit.fotocollector;
+
+/**
+ * Play around with Tika to extract and find images.
+ *
+ * @author hirsch
+ * @version 2016-02-19, 22:03
+ */
+public class FotoCollectorApplication {
+}
