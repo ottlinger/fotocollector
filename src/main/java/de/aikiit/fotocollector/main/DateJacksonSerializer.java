1129cb27eeaa66a54b5dcd1a0e494c316d07c053
Hugo Hirsch <github@hugo-hirsch.de>
2016-03-11 00:06:20 +0100

    Issue #1: Add JSON-Date parser.

---
+++ src/main/java/de/aikiit/fotocollector/main/DateJacksonSerializer.java
@@ -0,0 +1,10 @@
+package de.aikiit.fotocollector.main;
+
+import com.fasterxml.jackson.databind.JsonSerializer;
+
+/**
+ * @author hirsch
+ * @version 2016-03-10, 23:38
+ */
+public class DateJacksonSerializer extends JsonSerializer {
+}
