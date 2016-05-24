84eee09ea4b0fbf01e109dec27d12634cdf72c95
Hugo Hirsch <github@hugo-hirsch.de>
2016-05-24 07:04:13 +0200

    Issue #6: improve test.

--- src/test/java/de/aikiit/fotocollector/main/FileFinderTest.java
+++ src/test/java/de/aikiit/fotocollector/main/FileFinderTest.java
@@ -2,18 +2,31 @@ package de.aikiit.fotocollector.main;
 
 import de.aikiit.fotocollector.ScanResult;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.mockito.Matchers.contains;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author hirsch
  * @version 2016-05-23, 00:06
  */
+@RunWith(MockitoJUnitRunner.class)
 public class FileFinderTest {
+    @Mock
+    private PrintStream console;
 
     @Test
     public void emptyResultAndNullParameters() throws IOException {
+        System.setOut(console);
         FileFinder.scan(null);
         FileFinder.scan(new ScanResult());
+        verify(console, times(2)).println(contains("Nothing to do"));
     }
 }
