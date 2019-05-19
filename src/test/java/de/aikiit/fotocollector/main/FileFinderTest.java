4d5e9beb518d7c625fd34d777b53780fc5dd4edd
Hugo Hirsch <github@hugo-hirsch.de>
2019-05-19 21:14:17 +0200

    Issue #6: Update runner

--- src/test/java/de/aikiit/fotocollector/main/FileFinderTest.java
+++ src/test/java/de/aikiit/fotocollector/main/FileFinderTest.java
@@ -4,7 +4,6 @@ import de.aikiit.fotocollector.ScanResult;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.IOException;
 import java.io.PrintStream;
@@ -15,6 +14,7 @@ import static org.mockito.Mockito.verify;
 
 import org.junit.rules.TemporaryFolder;
 import org.junit.Rule;
+import org.mockito.junit.MockitoJUnitRunner;
 
 /**
  * @author hirsch
