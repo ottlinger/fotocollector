334a1e11dc9ceda3a29e47a9e41429c1c2b46c0a
Hugo Hirsch <github@hugo-hirsch.de>
2019-05-19 21:54:40 +0200

    Issue #7: Fix import

--- src/main/java/de/aikiit/fotocollector/main/DateJacksonSerializer.java
+++ src/main/java/de/aikiit/fotocollector/main/DateJacksonSerializer.java
@@ -1,7 +1,6 @@
 package de.aikiit.fotocollector.main;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
