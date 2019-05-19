b3ace613a121252c4d7ae61ff989535b8721a9fd
Hugo Hirsch <github@hugo-hirsch.de>
2019-05-19 21:10:33 +0200

    Issue #6: Add entry in lombok mode

--- src/main/java/de/aikiit/fotocollector/ScanEntry.java
+++ src/main/java/de/aikiit/fotocollector/ScanEntry.java
@@ -2,6 +2,7 @@ package de.aikiit.fotocollector;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import de.aikiit.fotocollector.main.DateJacksonSerializer;
+import lombok.Data;
 
 import java.util.Date;
 
@@ -11,9 +12,9 @@ import java.util.Date;
  * @author hirsch
  * @version 2016-02-20, 14:09
  */
+@Data
 public class ScanEntry {
 
-    // TODO use lombok to get rid of the boilerplate
     private final String fileName;
 
     /**
@@ -34,62 +35,9 @@ public class ScanEntry {
         this.creationDate = creationDate;
     }
 
-    public String getFileName() {
-        return fileName;
-    }
-
     @JsonSerialize(using=DateJacksonSerializer.class)
     public Date getCreationDate() {
         return creationDate;
     }
 
-    public long getSize() {
-        return size;
-    }
-
-    public void setSize(final long size) {
-        this.size = size;
-    }
-
-    public String getHashOverContent() {
-        return hashOverContent;
-    }
-
-    public void setHashOverContent(final String hashOverContent) {
-        this.hashOverContent = hashOverContent;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        final ScanEntry scanEntry = (ScanEntry) o;
-
-        if (size != scanEntry.size) return false;
-        if (fileName != null ? !fileName.equals(scanEntry.fileName) : scanEntry.fileName != null) return false;
-        if (hashOverContent != null ? !hashOverContent.equals(scanEntry.hashOverContent) : scanEntry.hashOverContent != null)
-            return false;
-        return creationDate != null ? creationDate.equals(scanEntry.creationDate) : scanEntry.creationDate == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = fileName != null ? fileName.hashCode() : 0;
-        result = 31 * result + (hashOverContent != null ? hashOverContent.hashCode() : 0);
-        result = 31 * result + (creationDate != null ? creationDate.hashCode() : 0);
-        result = 31 * result + (int) (size ^ (size >>> 32));
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "ScanEntry{" +
-                "fileName='" + fileName + '\'' +
-                ", hashOverContent='" + hashOverContent + '\'' +
-                ", creationDate=" + creationDate +
-                ", size=" + size +
-                '}';
-    }
 }
