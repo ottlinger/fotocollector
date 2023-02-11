5290a69d6beb7d5abde90d5151e5897c40670621
Hugo Hirsch <github@hugo-hirsch.de>
2023-02-11 20:21:02 +0100

    Remove piece of travis integration

--- travis-failure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Since we cannot reach the tests you may cat a problematic test here - run 'gradle test' locally to see its name."
-echo 
-echo "***** ALL TESTS *****"
-ls build/test-results/*.xml
-echo
-echo "***** A SPECIFIC ONE *****"
-cat build/test-results/TEST-de.aikiit.fotocollector.main.JsonOutputWriterTest.xml
